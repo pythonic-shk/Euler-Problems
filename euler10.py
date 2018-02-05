361039fe96a713d8fdfd2c0b3149465b52c880b3
Shakeel Javed <33480660+pythonic-shk@users.noreply.github.com>
2018-02-05 12:32:36 +0530

    Update euler10.py
    Used Eulerlib module to implement the sieve to fasten the problem of finding large primes.

--- euler10.py
+++ euler10.py
@@ -12,30 +12,30 @@
 			# primesum+=num
 			# print(primesum)
 # #print(primesum)
-primesum = 5
-primelist = [2,3]
-inc = 1
-while(1):
-	primesum += 6*inc - 1
-	primelist.append(6*inc - 1)
-	primesum += 6*inc + 1
-	primelist.append(6*inc + 1)
-	if 6*inc + 1 >= 1999999:
-		break
-	else:
-		inc+=1
-print(sum(primelist))
+#primesum = 5
+#primelist = [2,3]
+#inc = 1
+#while(1):
+#	primesum += 6*inc - 1
+#	primelist.append(6*inc - 1)
+#	primesum += 6*inc + 1
+#	primelist.append(6*inc + 1)
+#	if 6*inc + 1 >= 1999999:
+#		break
+#	else:
+#		inc+=1
+#print(sum(primelist))
 #print(primesum)
 #print(6*inc + 1)
 
-# import eulerlib
+ import eulerlib
 
 
-# # Call the sieve of Eratosthenes and sum the primes found.
-# def compute():
-	# ans = sum(eulerlib.prime_numbers.primes(1999999))
-	# return str(ans)
+Call the sieve of Eratosthenes and sum the primes found.
+def compute():
+	ans = sum(eulerlib.prime_numbers.primes(1999999))
+	return str(ans)
 
 
-# if __name__ == "__main__":
-	# print(compute())
+if __name__ == "__main__":
+	print(compute())
